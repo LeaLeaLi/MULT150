ad1e644fdc150071414000bf803c0aeccf9bb59a
LeaLeaLi <skk7623@proton.me>
2025-04-07 00:46:57 -0700

    Hour8

---
+++ Hour 8/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    int healthpoints = 3992;
+
+    void Start()
+    {
+        healthpoints = UsePotion(healthpoints);
+        healthpoints = UsePotion(healthpoints);
+        healthpoints = UsePotion(healthpoints);
+        healthpoints = UsePotion(healthpoints);
+
+        Debug.Log("Zell's Final Health: " + healthpoints);
+    }
+
+    void Update()
+    {
+    }
+
+    int UsePotion(int health)
+    {
+        return health + 400;
+    }
+}
