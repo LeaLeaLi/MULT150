f96786327c821bea32fb66c2a500538ad4ef2e59
LeaLeaLi <skk7623@proton.me>
2025-05-04 03:14:28 -0700

    Hour11LastUpdate

--- lli_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ lli_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -1,20 +1,25 @@
 using UnityEngine;
 
 public class PrefabGenerator : MonoBehaviour
-
 {
-
     public GameObject prefab;
+
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 spawnPosition = new Vector3(i * 3f, 0f, 0f);
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+    }
 
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.B))
-
         {
-
             Instantiate(prefab);
         }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(prefab, transform.position, transform.rotation);
