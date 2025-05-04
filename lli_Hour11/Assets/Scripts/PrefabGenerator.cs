d1e8fbb5529e8f7b3f9226be06b128cf9a6b9385
LeaLeaLi <skk7623@proton.me>
2025-05-04 02:24:21 -0700

    Hour11ExerciseUpdated

--- lli_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ lli_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 
 public class PrefabGenerator : MonoBehaviour
+
 {
+
     public GameObject prefab;
 
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.B))
+
         {
+
             Instantiate(prefab);
         }
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(prefab, transform.position, transform.rotation);
